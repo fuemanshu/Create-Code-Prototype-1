63a344063942daf149a81cd76c887195ba74d0ab
Fue Moua <fuemoua2022@gmail.com>
2025-12-04 20:31:13 -0600

    Added Player Input Control
    Player can now move left and right

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,7 +4,13 @@ public class PlayerController : MonoBehaviour
 {
 
     // Player movement speed
-    public float playerSpeed = 5.0f;
+    public float playerSpeed = 15.0f;
+
+    // player turn speed
+    public float playerTurnSpeed = 5.0f;
+
+    // player input variable for movement
+    public float playerHorizontalInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +21,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // player movement input
+        playerHorizontalInput = Input.GetAxis("Horizontal");
+
         // Move vehicle forward, at a reasonable realistic speed 
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);  // this version moves the player automatically
+
+        // player turn movement
+        transform.Translate(Vector3.right * Time.deltaTime * playerTurnSpeed * playerHorizontalInput);
     }
 }
